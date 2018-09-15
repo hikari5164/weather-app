7b1dd2d28926442dc50c96ba44f8ea330f1294ce
Hina <hmor664@aucklanduni.ac.nz>
2018-09-15 16:39:17 +1200

    Successfully connected to API

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,50 @@ import './App.css';
 
 import logo from './logo.svg';
 
-class App extends React.Component {
+interface IState {
+  data: any[],
+  results: any
+}
+
+export default class App extends React.Component<{}, IState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      data: [],
+      results: ""
+    };
+  }
+
+  public componentWillMount() {
+    this.getData();
+  }
+
+  public getData() {
+    fetch("https://ghibliapi.herokuapp.com/films?title=Castle%20in%20the%20Sky", {
+      method: "GET",
+      headers: {
+        "content-Type": "text/plain",
+      }
+    })
+    .then((response: any) => {
+      return response.json();
+    })
+    .then((data: any) => {
+      for (const element of data) {
+        const title = element.title;
+        document.getElementById("test")!.innerHTML += title;
+        console.log(title);
+      }
+    } );
+    //   if (!response.ok) {
+    //     this.setState({results: response.statusText})
+    //   }
+    //   else {
+    //     response.json().
+    //   }
+  }
+  
+
   public render() {
     return (
       <div className="App">
@@ -14,9 +57,9 @@ class App extends React.Component {
         <p className="App-intro">
           Hello World!
         </p>
+        <p>Test output</p>
+        <p id="test"/>
       </div>
     );
   }
 }
-
-export default App;
